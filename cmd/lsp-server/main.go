35142471e8be5c127d28ba384625ecc6abd1795f
SeleniaProject <menchan512@gmail.com>
2025-08-12 10:51:57 +0900

    cmd(lsp-server): add stdio LSP server entrypoint

---
+++ cmd/lsp-server/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "log"
+
+    "github.com/orizon-lang/orizon/internal/tools/lsp"
+)
+
+func main() {
+    if err := lsp.RunStdio(); err != nil {
+        log.Fatal(err)
+    }
+}
+
+
